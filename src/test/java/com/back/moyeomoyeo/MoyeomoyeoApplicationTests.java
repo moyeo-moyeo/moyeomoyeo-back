4637c622b09b9e3130d31d73a55e8dc77a5800f9
cousim46 <wjdghldns24@naver.com>
2022-07-09 15:36:06 +0900

    프로젝트 세팅

---
+++ src/test/java/com/back/moyeomoyeo/MoyeomoyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.moyeomoyeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeomoyeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
