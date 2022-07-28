0c6f460432d11ea5b5c62e6e5cf174cbad4d9d38
cousim46 <wjdghldns24@naver.com>
2022-07-29 01:11:30 +0900

    Add : memberRepositorty 유닛테스트

--- src/test/java/com/back/moyeomoyeo/repository/member/MemberRepositoryCustomTest.java
+++ src/test/java/com/back/moyeomoyeo/repository/member/MemberRepositoryCustomTest.java
@@ -1,82 +1,63 @@
 package com.back.moyeomoyeo.repository.member;
 
 import com.back.moyeomoyeo.entity.member.Member;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static com.back.moyeomoyeo.entity.member.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberRepositoryCustomTest {
 
-
-    @Autowired
-    private JPAQueryFactory queryFactory;
-
     @Autowired
     private MemberRepository memberRepository;
 
     @Test
-    void 가입된_닉네임_또는_아이디가_없을경우_True_반환합니다() {
+    void 가입된_닉네임_또는_아이디가_없을경우_False_반환합니다() {
         //given
         String loginId = "hoe";
         String nickname = "아으닉네임";
 
         //when
-        Member findMember = queryFactory.selectFrom(member)
-                .where(member.loginId.eq(loginId).or(member.nickname.eq(nickname)))
-                .fetchFirst();
-
-        boolean isExists = findMember == null;
+        boolean isExists = memberRepository.existsMemberLoginIdOrNickname(loginId, nickname);
 
         //then
-        assertThat(isExists).isTrue();
+        assertThat(isExists).isFalse();
 
     }
 
     @Test
-    void 가입된_닉네임_존재할경우_False_반환() {
+    void 가입된_닉네임_존재할경우_True_반환() {
         //given
         String nickname = "아으닉네임";
         memberRepository.save(
                 new Member("hoe", "1234", "hello",
                         "아으닉네임", "19981015", "01012341234"));
         //when
-        Member findMember = queryFactory.selectFrom(member)
-                .where(member.nickname.eq(nickname)).fetchFirst();
-
-        boolean existsIsTrue = findMember == null;
+        boolean isExists = memberRepository.existsNickname(nickname);
 
         //then
-        assertThat(existsIsTrue).isFalse();
-        assertThat(findMember.getNickname()).isEqualTo(nickname);
+        assertThat(isExists).isTrue();
     }
 
     @Test
-    void 가입된_로그인아이디가_존재하지않을경우_True_반환() {
+    void 가입된_로그인아이디가_존재하지않을경우_False_반환() {
 
         //given
         String loginId = "test12";
 
         //when
-        Member findMember = queryFactory.selectFrom(member)
-                .where(member.loginId.eq(loginId))
-                .fetchFirst();
-
-        boolean isExists = findMember == null;
+        boolean isExists = memberRepository.existsLoginId(loginId);
 
         //then
-        assertThat(isExists).isTrue();
-        assertThat(findMember).isNull();
+        assertThat(isExists).isFalse();
     }
 
     @Test
-    void 가입된_로그인아이디가_존재하지할경우_False_반환() {
+    void 가입된_로그인아이디가_존재하지할경우_True_반환() {
 
         //given
         String loginId = "test12";
@@ -85,15 +66,10 @@ class MemberRepositoryCustomTest {
                         "아으닉네임", "19981015", "01012341234"));
 
         //when
-        Member findMember = queryFactory.selectFrom(member)
-                .where(member.loginId.eq(loginId))
-                .fetchFirst();
-
-        boolean isExists = findMember == null;
+        boolean isExists = memberRepository.existsLoginId(loginId);
 
         //then
-        assertThat(isExists).isFalse();
-        assertThat(findMember.getLoginId()).isEqualTo(loginId);
+        assertThat(isExists).isTrue();
     }
 
     @Test
@@ -106,10 +82,7 @@ class MemberRepositoryCustomTest {
                         "아으닉네임", "19981015", "01012341234"));
 
         //when
-        Member findMember = queryFactory
-                .selectFrom(member)
-                .where(member.nickname.eq(nickname))
-                .fetchOne();
+        Member findMember = memberRepository.findByNickname(nickname);
 
         //then
         assertThat(findMember.getNickname()).isEqualTo(nickname);
