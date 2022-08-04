ee3871b247e8b4b93e7627543263627dea8b40a4
choikangheon <kangheon3@gmail.com>
2022-08-04 22:53:16 +0900

    추가 :  Redis 환경구축   - 테스트코드 (인메모리 Hash데이터 입출력)

---
+++ src/main/java/com/back/moyeomoyeo/entity/tempNumber/TempNumber.java
@@ -0,0 +1,21 @@
+/*
+package com.back.moyeomoyeo.entity.tempNumber;
+
+
+import com.back.moyeomoyeo.entity.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Setter
+public class TempNumber  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Member owner;
+}
+*/
---
+++ src/main/java/com/back/moyeomoyeo/repository/tempNumber/TempNumberRepository.java
@@ -0,0 +1,10 @@
+/*
+package com.back.moyeomoyeo.repository.tempNumber;
+
+import com.back.moyeomoyeo.entity.tempNumber.TempNumber;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TempNumberRepository extends CrudRepository<TempNumber,Long> {
+
+}
+*/
