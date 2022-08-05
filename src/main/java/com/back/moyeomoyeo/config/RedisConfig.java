2aec661a125d0ec83b189a51c6b4bb989eea5f86
choikangheon <kangheon3@gmail.com>
2022-08-05 22:29:02 +0900

    수정 :  MemberUpdatePasswordRequest 리팩토링   - Getter Setter -> lombok

--- src/main/java/com/back/moyeomoyeo/config/RedisConfig.java
+++ src/main/java/com/back/moyeomoyeo/config/RedisConfig.java
@@ -7,10 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories
 @Getter
 public class RedisConfig {
     @Value("${spring.redis.host}")
--- src/main/java/com/back/moyeomoyeo/dto/member/request/MemberUpdatePasswordRequest.java
+++ src/main/java/com/back/moyeomoyeo/dto/member/request/MemberUpdatePasswordRequest.java
@@ -1,33 +1,16 @@
 package com.back.moyeomoyeo.dto.member.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MemberUpdatePasswordRequest {
     private String beforePassword;
     private String afterPassword;
     private String repeatPassword;
 
-    public String getBeforePassword() {
-        return beforePassword;
-    }
 
-    public void setBeforePassword(String beforePassword) {
-        this.beforePassword = beforePassword;
-    }
-
-    public String getAfterPassword() {
-        return afterPassword;
-    }
-
-    public void setAfterPassword(String afterPassword) {
-        this.afterPassword = afterPassword;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
-    }
-
-    public void setRepeatPassword(String repeatPassword) {
-        this.repeatPassword = repeatPassword;
-    }
 
     public MemberUpdatePasswordRequest(String beforePassword, String afterPassword, String repeatPassword) {
         this.beforePassword = beforePassword;
