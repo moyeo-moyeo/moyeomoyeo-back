0bc5ff06c84dd80f6a72df653eaf43285847b106
cousim46 <wjdghldns24@naver.com>
2022-08-10 02:08:09 +0900

    Fix : Controller로부터 들어오는 값 변경

--- src/main/java/com/back/moyeomoyeo/repository/friend/FriendApproveRepository.java
+++ src/main/java/com/back/moyeomoyeo/repository/friend/FriendApproveRepository.java
@@ -4,4 +4,5 @@ import com.back.moyeomoyeo.entity.friend.FriendApprove;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FriendApproveRepository extends JpaRepository<FriendApprove, Long> {
+    FriendApprove findByRequestNickname(String nickname);
 }
