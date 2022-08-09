233d9ac13e5c55b847b9ad5f3c33c219bbf2543c
cousim46 <wjdghldns24@naver.com>
2022-08-10 00:46:26 +0900

    Add : 친구 요청에 대한 유닛테스트

--- src/test/java/com/back/moyeomoyeo/service/friend/FriendServiceTest.java
+++ src/test/java/com/back/moyeomoyeo/service/friend/FriendServiceTest.java
@@ -2,104 +2,141 @@ package com.back.moyeomoyeo.service.friend;
 
 import com.back.moyeomoyeo.dto.friend.request.NewFriendRequest;
 import com.back.moyeomoyeo.dto.friend.response.NewFriendResponse;
+import com.back.moyeomoyeo.entity.friend.Friend;
+import com.back.moyeomoyeo.entity.friend.FriendApprove;
+import com.back.moyeomoyeo.entity.friend.friendenum.FriendApproveEnum;
 import com.back.moyeomoyeo.entity.member.Member;
-import com.back.moyeomoyeo.errorhandle.member.ErrorCode;
 import com.back.moyeomoyeo.errorhandle.member.ErrorException;
 import com.back.moyeomoyeo.repository.friend.FriendApproveRepository;
 import com.back.moyeomoyeo.repository.friend.FriendRepository;
 import com.back.moyeomoyeo.repository.friend.FriendRepositoryCustom;
 import com.back.moyeomoyeo.repository.member.MemberRepository;
 import com.back.moyeomoyeo.security.AuthorizedUser;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
+@SpringBootTest
+@Transactional
 class FriendServiceTest {
 
-
-    @InjectMocks
-    @Spy
+    @Autowired
     private FriendService friendService;
-    @Mock
+    @Autowired
     private FriendApproveRepository friendApproveRepository;
-    @Mock
+    @Autowired
     private FriendRepository friendRepository;
-    @Mock
+    @Autowired
     private FriendRepositoryCustom friendRepositoryCustom;
-    @Mock
+    @Autowired
     private MemberRepository memberRepository;
 
-    @BeforeEach
-    public void setMemberUp() {
-        memberRepository.save(new Member("test", "1234", "hoe", "아으닉네임", "981212", "01011112222"));
-        memberRepository.save(new Member("test12", "1234", "hoe12", "아으닉네임12", "981212", "01011112222"));
-    }
-
 
     @Test
-    void 로그인한_사용자가_입력한닉네임을_가진유저에게_친구요청_성공시_친구요청완료메시지_반환() {
+    void 로그인한사용자가_입력한닉네임이_존재하지않으면_ErrorException_발생() {
+
         //given
         Member member = new Member("test", "1234", "hoe",
-                "아으닉네임", "981212", "01011112222");
-        AuthorizedUser loginMember = new AuthorizedUser(member);
-        //when
-        NewFriendResponse newFriendResponse = new NewFriendResponse("친구 요청 완료");
-        doReturn(newFriendResponse).when(friendService).newFriendRequest(loginMember, new NewFriendRequest("아으닉네임12"));
-        //then
-        assertThat(newFriendResponse.getMessage()).isEqualTo("친구 요청 완료");
-
-    }
-
-    @Test
-    void 로그인한_사용자가_입력한닉네임이_존재하지않을경우_ErrorException_발생() {
-        //given
-        Member member = new Member("test", "1234", "hoe",
-                "아으닉네임", "981212", "01011112222");
+                "아으닉네임", "981015", "01012341234");
+        memberRepository.save(member);
         AuthorizedUser loginMember = new AuthorizedUser(member);
 
         //when
-        doThrow(new ErrorException(ErrorCode.NOT_EXISTS_USER)).when(friendService).newFriendRequest(loginMember, new NewFriendRequest("없음"));
-        ErrorCode response = assertThrows(ErrorException.class, () ->
-                friendService.newFriendRequest(loginMember, new NewFriendRequest("없음"))).getErrorCode();
+        ErrorException errorResponse = assertThrows(ErrorException.class, () ->
+                friendService.newFriendRequest(loginMember, new NewFriendRequest("닉네임")));
 
         //then
-        verify(friendService, atLeastOnce()).newFriendRequest(any(), any());
-        assertThat(response.getMessage()).isEqualTo("존재하지 않는 유저입니다.");
-        assertThat(response.getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(errorResponse.getErrorCode().getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(errorResponse.getErrorCode().getMessage()).isEqualTo("존재하지 않는 유저입니다.");
 
     }
 
     @Test
-    void 로그인한_사용자가_입력한닉네임이_자기자신이면_ErrorException_발생() {
+    void 로그인한사용자가_입력한닉네임이_자기자신이면_ErrorException_발생() {
         //given
         Member member = new Member("test", "1234", "hoe",
-                "아으닉네임", "981212", "01011112222");
+                "아으닉네임", "981015", "01012341234");
+        memberRepository.save(member);
         AuthorizedUser loginMember = new AuthorizedUser(member);
 
         //when
-        doThrow(new ErrorException(ErrorCode.NOT_ADD_MYSELF)).when(friendService).newFriendRequest(loginMember, new NewFriendRequest("아으닉네임"));
-        ErrorCode responseError = assertThrows(ErrorException.class, () ->
-                friendService.newFriendRequest(loginMember, new NewFriendRequest("아으닉네임"))).getErrorCode();
+
+        ErrorException errorResponse = assertThrows(ErrorException.class, () ->
+                friendService.newFriendRequest(loginMember, new NewFriendRequest("아으닉네임")));
+
         //then
-
-        verify(friendService, atLeastOnce()).newFriendRequest(any(), any());
-        assertThat(responseError.getMessage()).isEqualTo("본인을 친구 추가할 수 없습니다.");
-        assertThat(responseError.getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
-
+        assertThat(errorResponse.getErrorCode().getMessage()).isEqualTo("본인을 친구 추가할 수 없습니다.");
+        assertThat(errorResponse.getErrorCode().getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
+    @Test
+    void 로그인한사용자가_입력한닉네임이_이미_요청한닉네임이면_ErrorException_발생() {
+        //given
+        Member member = new Member("test", "1234", "hoe",
+                "아으닉네임", "981015", "01012341234");
+        Member requestMember = new Member("test12", "1234", "hi",
+                "제2의인생", "981015", "01012341234");
+        memberRepository.save(member);
+        memberRepository.save(requestMember);
+        AuthorizedUser loginMember = new AuthorizedUser(member);
+
+        //when
+        friendService.newFriendRequest(loginMember, new NewFriendRequest("제2의인생"));
+        ErrorException errorResponse = assertThrows(ErrorException.class, () ->
+                friendService.newFriendRequest(loginMember, new NewFriendRequest("제2의인생")));
+
+        //then
+        assertThat(errorResponse.getErrorCode().getMessage()).isEqualTo("이미 친구 요청을 보낸 사용자입니다.");
+        assertThat(errorResponse.getErrorCode().getHttpStatus()).isEqualTo(HttpStatus.CONFLICT);
+    }
+
+    @Test
+    void 로그인한사용자가_입력한닉네임이_이미_친구추가된_사용자일경우_ErrorException_발생() {
+        //given
+        Member member = new Member("test", "1234", "hoe",
+                "아으닉네임", "981015", "01012341234");
+        Member requestMember = new Member("test12", "1234", "hi",
+                "제2의인생", "981015", "01012341234");
+        memberRepository.save(member);
+        memberRepository.save(requestMember);
+        AuthorizedUser loginMember = new AuthorizedUser(member);
+        friendRepository.save(new Friend(requestMember, member.getNickname()));
+
+        //when
+        ErrorException errorResponse = assertThrows(ErrorException.class, () ->
+                friendService.newFriendRequest(loginMember, new NewFriendRequest(requestMember.getNickname())));
+
+        //then
+        assertThat(errorResponse.getErrorCode().getMessage()).isEqualTo("이미 친구 추가된 사용자입니다.");
+        assertThat(errorResponse.getErrorCode().getHttpStatus()).isEqualTo(HttpStatus.CONFLICT);
+    }
+
+    @Test
+    void 로그인한사용자가_입력한닉네임으로_친구요청() {
+        //given
+        Member member = new Member("test", "1234", "hoe",
+                "아으닉네임", "981015", "01012341234");
+        Member requestMember = new Member("test12", "1234", "hi",
+                "제2의 인생", "981015", "01012341234");
+        memberRepository.save(requestMember);
+        memberRepository.save(member);
+        AuthorizedUser loginMember = new AuthorizedUser(member);
+
+        //when
+        NewFriendResponse friendRequestResponse = friendService.newFriendRequest(loginMember, new NewFriendRequest("제2의 인생"));
+        Optional<FriendApprove> requestSendMember = friendApproveRepository.findById(member.getId());
+        //then
+        assertThat(friendRequestResponse.getMessage()).isEqualTo("친구 요청 완료");
+        assertThat(requestSendMember.orElseGet(() -> null).getIsApprove()).isEqualTo(FriendApproveEnum.REQUEST);
+
+
+    }
 
 }
