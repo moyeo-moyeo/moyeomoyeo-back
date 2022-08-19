5e142de349b16c61ea6fedfdd4f1206031c6ad03
cousim46 <wjdghldns24@naver.com>
2022-08-19 19:56:46 +0900

    Fix : 병합 충돌 해결

--- src/main/java/com/back/moyeomoyeo/BaseEntity.java
+++ src/main/java/com/back/moyeomoyeo/BaseEntity.java
@@ -11,12 +11,10 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
-
     @CreatedDate
     private LocalDateTime createdDate;
 
     // 조회한 Entity 값을 변경할 때 시간이 자동 저장됩니다.
     @LastModifiedDate
     private LocalDateTime modifiedDate;
-
 }
