49966e1ed942b345ce223b7142f9d52344b39254
cousim46 <wjdghldns24@naver.com>
2022-10-09 19:25:36 +0900

    Add : 약속에 대한 생성시간, 수정시간에 대한 속성 추가

--- src/main/java/com/back/moyeomoyeo/entity/promise/Promise.java
+++ src/main/java/com/back/moyeomoyeo/entity/promise/Promise.java
@@ -1,5 +1,6 @@
 package com.back.moyeomoyeo.entity.promise;
 
+import com.back.moyeomoyeo.BaseEntity;
 import com.back.moyeomoyeo.entity.member.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Promise {
+public class Promise extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,5 +31,12 @@ public class Promise {
     public Promise(PromisePlace promisePlace, Member member) {
         this.promisePlace = promisePlace;
         this.member = member;
+    }
+
+    public Promise(PromisePlace promisePlace, Member member, Integer latitude, Integer longitude) {
+        this.promisePlace = promisePlace;
+        this.member = member;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 }
