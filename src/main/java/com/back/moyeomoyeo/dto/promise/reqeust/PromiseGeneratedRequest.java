5d1809b1aab87e73bcda0d523cab31b20ac73349
cousim46 <wjdghldns24@naver.com>
2022-10-09 19:27:13 +0900

    Add : 약속 장소 요청 dto

--- src/main/java/com/back/moyeomoyeo/dto/promise/reqeust/PromiseGeneratedRequest.java
+++ src/main/java/com/back/moyeomoyeo/dto/promise/reqeust/PromiseGeneratedRequest.java
@@ -4,21 +4,27 @@ import com.back.moyeomoyeo.entity.promise.PromisePlace;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PromiseGeneratedRequest {
+    @NotNull(message = "약속장소를 필수로 입력해주세요.")
     private String placeName; // 장소이름
 
+    @NotNull(message = "위도가 필수적으로 필요합니다.")
     private Integer latitude; // 위도
 
-
+    @NotNull(message = "경도가 필수적으로 필요합니다.")
     private Integer longitude; // 경도
 
-    private String meetingDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime meetingDate;
 
     private List<String> friendNicknameList;
 
